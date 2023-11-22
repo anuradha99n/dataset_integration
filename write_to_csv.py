27d036193dde79e767a062e659331dc5a2d93475
Anuradha Nayanajith <65272187+anuradha99n@users.noreply.github.com>
2023-11-22 21:33:47 +0530

    Update write_to_csv.py
    Add BPD stages to write

--- write_to_csv.py
+++ write_to_csv.py
@@ -2,14 +2,14 @@ import csv
 def write_to_csv(output_file, files_and_data):
     try:
         with open(output_file, 'w', encoding='utf-8', newline='') as csvfile:
-            fieldnames = ['Data_ID'] + list(files_and_data[0][1].keys())
+            fieldnames = ['Sample_ID'] + list(files_and_data[0][1].keys()) + ['BPD_stage']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
             for file_data in files_and_data:
-                row_data = {'Data_ID': file_data[0], **file_data[1]}
+                row_data = {'Sample_ID': file_data[0], **file_data[1], 'BPD_stage': file_data[2]}
                 writer.writerow(row_data)
-                print(f"{file_data[0]} : Done")
+                # print(f"{file_data[0]} : Done")
                 
     except PermissionError:
         print(f"Permission error: {output_file}")
